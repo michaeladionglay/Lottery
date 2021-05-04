f10503ef2c15dbbdaef6641596ec8b9bb40dce03
Omarnabulsi <56372737+Omarnabulsi@users.noreply.github.com>
2021-05-04 14:07:41 -0500

    Added the class for getting $1 games from website.

--- app/src/main/java/com/example/lottery/MainActivity.java
+++ app/src/main/java/com/example/lottery/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.lottery;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    static TextView $1Games;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        $1Games = findViewById(R.id.listOf1Games);
+        Button getListOf$1Games = findViewById(R.id.getListOf1Games);
+
+        getListOf$1Games.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new getListOf$1Games().execute();
+            }
+        });
     }
 }
