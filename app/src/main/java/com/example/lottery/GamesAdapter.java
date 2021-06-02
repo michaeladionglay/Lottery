8947e8ac15be67e650bcb2eaa848854b2e364063
aza <aza21aya@gmail.com>
2021-06-01 21:23:14 -0500

    restructured the main page so text lies more horizontal. Still not sure why the separator is lightened most the time?! colors might be too vibrant. Added a header line

--- app/src/main/java/com/example/lottery/GamesAdapter.java
+++ app/src/main/java/com/example/lottery/GamesAdapter.java
@@ -40,8 +40,8 @@ public class GamesAdapter extends RecyclerView.Adapter<MyViewHolder> {
         Game game = gameList.get(position);
 
         holder.ticketName.setText(game.getName());
-        holder.allprizesRemain.setText(String.format("%.0f",game.getPercentage_all())+"% All Prizes Remaining");
-        holder.grandprizeRemain.setText(String.format("%.0f",game.getPercentage_grand())+"% Grand Prizes Remaining");
+        holder.allprizesRemain.setText(String.format("%.0f",game.getPercentage_all())+"% Winning Tickets");
+        holder.grandprizeRemain.setText(String.format("%.0f",game.getPercentage_grand())+"% Grand Prize Tickets");
         //holder.empId.setText(String.format(Locale.getDefault(), "%d", employee.getEmpId()));
         //holder.department.setText(employee.getDepartment());
         //holder.dateTime.setText(new Date().toString());
